1db969239c426eae55bbbd4331f8d36dbeb8923f
user <kosenko2401@gmail.com>
2024-12-28 21:16:36 +0200

    Rename project

---
+++ src/quiz/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Services(models.Model):
+    ...
+
+class Results(models.Model):
+    ...
+
+class Appointments(models.Model):
+    ...
+
+class Bills(models.Model):
+    ...
